edcf35e12f92b55b9747b7aa94c916b4eed72525
karthikkaraka <karthikkaraka444@gmail.com>
2026-03-25 17:36:38 +0530

    this is first commit

---
+++ src/main/java/com/Sridevi/Campushelpdesk/CampushelpdeskApplication.java
@@ -0,0 +1,13 @@
+package com.Sridevi.Campushelpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampushelpdeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampushelpdeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Sridevi/Campushelpdesk/CampushelpdeskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sridevi.Campushelpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampushelpdeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
